c77a791480a9899716180abfd5655d8bc1e12573
Max Taldykin <jorpic@gmail.com>
2020-03-24 08:57:30 +0300

    Adjustable grid size

--- src/index.js
+++ src/index.js
@@ -9,9 +9,11 @@ class App extends Component {
     super();
     this.setState({
       codeChanged: true,
+      gridSize: localStorage.getItem("gridSize") || 30,
       time: 0
     });
     this.codeRef = createRef();
+    this.onGrid = this.onGrid.bind(this);
     this.onCode = this.onCode.bind(this);
     this.eval = this.eval.bind(this);
   }
@@ -38,12 +40,19 @@ class App extends Component {
     return {pixels, messages};
   }
 
+
+  onGrid(e) {
+    const gridSize = e.target.value;
+    localStorage.setItem("gridSize", gridSize);
+    this.setState({gridSize});
+  }
+
   onCode(e) {
     this.setState({codeChanged: true});
   }
 
   render() {
-    const grid = 40;
+    const grid = this.state.gridSize;
 
     const textarea = this.codeRef.current;
     const code = textarea ? textarea.value : "";
@@ -51,31 +60,38 @@ class App extends Component {
     const log = res.messages.join("\n");
     const pixels = res.pixels.map(({x, y, color}) =>
       <rect
-        x={x*grid + 1}
-        y={y*grid + 1}
+        x={x*grid + 0.5}
+        y={y*grid + 0.5}
         width={grid-1}
-        height={grid-2}
+        height={grid-1}
         fill={color || "skyblue"}/>
     );
+    const rect = (size, width) =>
+      <rect width={size} height={size} fill="none" stroke="lightgray" stroke-width={width}/>;
+    const gridPattern =
+      <defs>
+        <pattern id="smallGrid" width={grid} height={grid} patternUnits="userSpaceOnUse">
+          {rect(grid, 0.5)}
+        </pattern>
+        <pattern id="grid" width={grid*5} height={grid*5} patternUnits="userSpaceOnUse">
+          <rect width={grid*5} height={grid*5} fill="url(#smallGrid)"/>
+          {rect(grid*5, 1)}
+        </pattern>
+      </defs>;
     return (
       <div class="columns">
         <div class="left">
-          <div class="env"></div>
+          <div class="env">
+            <div>Размер клеток: {grid}</div>
+            <input type="range" min="8" max="60" value={grid} onChange={this.onGrid}/>
+          </div>
           <textarea class="code" onKeyUp={this.onCode} ref={this.codeRef}>
           </textarea>
           <textarea class="log" readonly="true" value={log}></textarea>
         </div>
         <div class="right">
           <svg width="100%" height="100%">
-            <defs>
-              <pattern id="smallGrid" width="40" height="40" patternUnits="userSpaceOnUse">
-                <path d="M 40 0 L 0 0 0 40" fill="none" stroke="lightgray" stroke-width="0.5"/>
-              </pattern>
-              <pattern id="grid" width="200" height="200" patternUnits="userSpaceOnUse">
-                <rect width="200" height="200" fill="url(#smallGrid)"/>
-                <path d="M 200 0 L 0 0 0 200" fill="none" stroke="lightgray" stroke-width="1"/>
-              </pattern>
-            </defs>
+            {gridPattern}
             <rect width="100%" height="100%" fill="url(#grid)" />
             {pixels}
           </svg>
